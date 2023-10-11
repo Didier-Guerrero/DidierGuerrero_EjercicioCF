af394df49affa8b024d109e8b5529254b390e8d1
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-10-11 08:51:29 -0500

    Adicion de la tabla

---
+++ Models/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DidierGuerrero_EjercicioCF.Models
+{
+    public class Burger
+    {
+        public int ID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 99.99)]
+        public decimal Price { get; set; }
+    }
+}
